e1be4c10fa567a4c1661a3f83cb195bfc68b0ad0
Matti Kiviharju <matti.kiviharju@i4ware.fi>
2026-06-07 09:46:44 +0300

    Initial.

--- saas-app/resources/views/emails/showresetpassword.blade.php
+++ saas-app/resources/views/emails/showresetpassword.blade.php
@@ -2,3 +2,4 @@
 <p>This is automated email. Do not response for this email.</p>
 <h1>Your Password is Changed</h1>
 <p>Please login with new password:</p>
+<p><a clicktracking=off href="{{ env('APP_UI_URL') }}/#/login">Login</a></p>
